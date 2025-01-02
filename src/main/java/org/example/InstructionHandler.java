a5ecf0898609114b6260c7051cd2ed423b9016b0
Sean Kembo <kembosean7@gmail.com>
2025-01-02 18:14:35 +0200

    Handle invalid move command input by adding error handling for missing or malformed distance values

--- src/main/java/org/example/InstructionHandler.java
+++ src/main/java/org/example/InstructionHandler.java
@@ -8,6 +8,22 @@ public class InstructionHandler {
         double x = 0, y = 0;
         double orientation = 0;
 
-        System.out.printf("I'm at (%.2f, %.2f) facing %.2f degrees%n", x, y, orientation);;
+        System.out.printf("I'm at (%.2f, %.2f) facing %.2f degrees%n", x, y, orientation);
+
+
+    }
+    private static double[] moveCommand(int i, double orientation, String instruction, double x, double y){
+
+        String[] moveCommand = instruction.split(" ");
+        try {
+            double distance = Double.parseDouble(moveCommand[1]);
+            distance = Math.round(distance * 100.0) / 100.0;
+        }
+        catch (IndexOutOfBoundsException | NumberFormatException e){
+            System.out.printf("I've encountered an instruction I don't understand, aborting instruction %d", i);
+            return new double[] {x,y};
+        }
+
+
     }
 }
