7d6446e5e30a7e5c066a7144c76e0b816408be5f
Sean Kembo <kembosean7@gmail.com>
2025-01-04 21:30:04 +0200

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,6 +11,6 @@ public class Main {
         readFile readFile = new readFile();
         List<String> data =readFile.getInstructions("instructions.txt");
         InstructionHandler.instructionMovements(data);
-//        System.out.println(data);
+
     }
 }
